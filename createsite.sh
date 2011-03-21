51d32e9d8c4792573c6dcd5cba5211a11eac1891
moba <moritz@headstrong.de>
2011-03-21 01:39:29 -0400

    fixed link to github

--- createsite.sh
+++ createsite.sh
@@ -11,11 +11,11 @@ fi
 
 mkdir /var/www/sites/$1
 cd /var/www/sites/$1
-git clone git@github.com:moba/dokuwiki.git htdocs
+git clone git://github.com/moba/dokuwiki.git htdocs
 cd htdocs
 git remote add upstream https://github.com/splitbrain/dokuwiki.git
 git checkout -b stable origin/stable 
 mkdir data/cache # git doesn't track empty dirs
 cd lib/tpl/
-git clone git@github.com:moba/dokuwiki-minimal.git minimal
+git clone git://github.com/moba/dokuwiki-minimal.git minimal
 
